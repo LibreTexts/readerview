6da56e544c066fa26a7a73561d1207eda1ca5717
Yasin Dahi <ymdahi@gmail.com>
2022-08-30 22:12:03 -0700

    set active toc item

--- modules/contents.js
+++ modules/contents.js
@@ -1,3 +1,5 @@
+// import { Page } from "./pageinfo";
+
 /**
  * Renders the Table of Contents into the off-canvas menu.
  *
@@ -7,25 +9,33 @@ async function renderTableOfContents() {
   const { structured } = await LibreTexts.getTOC();
   const container = document.getElementById('offcanvas-menu');
   const [subdomain] = LibreTexts.parseURL();
+  /**
+   * ToDo:
+   * - set current page id in LibreTexts object
+   */
+  const currentPageId = document.getElementById('IDHolder').innerText;
 
   if (!structured || !container) {
     console.error('[ReaderView]: Error building Table of Contents.');
     return;
   }
-
   const buildListView = (page) => {
     if (Array.isArray(page.subpages)) {
       const children = page.subpages.map((item) => {
         const grandchildren = buildListView(item);
         return `
-          <li>
+          <li class="${((currentPageId == item.id) ? `m-${item.id} active` : `m-${item.id}`)}">
             <span>
-              <a href="https://${subdomain}.libretexts.org/${item.path}?readerView">${item.title}</a>
+              <a href="/${item.path}?readerView">${item.title}</a>
             </span>
             ${grandchildren}
           </li>
         `;
       }).filter((item) => item !== null);
+      /** 
+       * ToDo:
+       * - set current page's parent ul as active if current page is subpage
+       */
       if (children.length > 0) {
         return `
           <ul>
