d5606325abaade8c47aea7510201897b902e0fed
Yasin Dahi <ymdahi@gmail.com>
2022-10-25 14:56:46 -0400

    video pending

--- modules/help.js
+++ modules/help.js
@@ -3,16 +3,21 @@ function renderHelp() {
   //const docsURL = 'https://studio.libretexts.org/h5p/10963/embed'
   const additionalInfo = `
     <div id="helpInfo">
+      <h3>Video Overview</h3>
+      <p>Video is being build and will be available soon.</p>
+      <p>Please contact <a href="mailto:info@libretexts.org">info@libretexts.org</a> if you have any questions.</p>
       <h3>Still have questions?</h3>
       <p>You should first contact your instructor regarding any issues with this book if it assigned reading.</p>
       <p>If you are still running into issues or have questions, please send your query to <a href="mailto:info@libretexts.org">info@libretexts.org</a>.</p>
       <p>You can also refer to the <a href="#" target="_blank">LibreTexts Documentation</a> for tutorials and guides on using the LibreTexts platform.
     </div>
   `;
+  return additionalInfo;
+  /*
   return `
   <iframe src="https://studio.libretexts.org/h5p/10963/embed" width="980" height="612" frameborder="0" allowfullscreen="allowfullscreen" lang="en"></iframe><script src="https://studio.libretexts.org/modules/contrib/h5p/vendor/h5p/h5p-core/js/h5p-resizer.js" charset="UTF-8"></script>
   ${additionalInfo}
-  `;
+  `;*/
 }
 
 export { renderHelp };
