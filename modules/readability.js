e4e6e3569bbd090b48404ab472a55d8530f31a9b
ShreyaShinde25 <shreyarshinde@gmail.com>
2024-03-05 19:44:54 -0800

    readability layout

---
+++ modules/readability.js
@@ -0,0 +1,9 @@
+
+function readability_panel(){
+   console.log("Hello there");
+   document.getElementById('readability').innerHTML = `
+   <img title="Beeline Logo" src="https://test.libretexts.org/hagnew/development/public/Binh%20Nguyen/ReactSidebar/src/assets/beeline_logo_combo_master-cropped.svg" style="margin: 0px 5vw;">
+   `;
+
+}
+export { readability_panel };
